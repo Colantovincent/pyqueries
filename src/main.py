4a825e52b86e1c6dfa4a20403e548f1aaf2ba856
colantovincent <colantoniovincent2005@gmail.com>
2025-09-29 17:57:24 +0200

    Refactor query handling to use SQLPart for parameterization and improve where clause management

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     persona = table.Table("persona")
     nome = persona.register_field("nome", **{"size": 45, "sql_type": "VARCHAR", "nullable": True})
     eta = persona.register_field("eta", **{"size": 3, "sql_type": "INT", "nullable": True})
-    myQuery.select([persona.fields["nome"]], persona).where(persona.fields["nome"] == "`Mario\"' AND 1 = 1").where(persona.fields["eta"] != 1).where(eta > 0)
+    myQuery.select([persona.fields["nome"]], persona).where(persona.fields["nome"] == "Mario").where(eta < 3).where(nome != None)
 
-    print(myQuery.get_string())
+    sql, params = myQuery.get_string()
+    print("Query:\t", sql, "\nParams:\t", params)
